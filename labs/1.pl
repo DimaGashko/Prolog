37dc3602bcc6de4773dc7462f2036f4010e767a4
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-11-28 23:59:24 +0200

    l1

--- labs/1.pl
+++ labs/1.pl
@@ -1,1 +1,44 @@
-mother-in-law
+man(oliver).
+man(jake).
+man(noah).
+man(james).
+man(jack).
+man(connor).
+man(liam).
+man(john).
+
+woman(amelia).
+woman(margaret).
+woman(emma).
+woman(mary).
+woman(emily).
+woman(isabella).
+woman(olivia).
+woman(samantha).
+
+couple(oliver, amelia).
+couple(jake, margaret).
+couple(john, emma).
+couple(noah, mary).
+couple(james, emily).
+couple(isabella, jack).
+couple(olivia, connor).
+
+parent(oliver, noah).
+parent(amelia, noah).
+parent(jake, mary).
+parent(margaret, mary).
+parent(john, isabella).
+parent(emma, isabella).
+parent(noah, jack).
+parent(noah, olivia).
+parent(mary, jack).
+parent(mary, olivia).
+parent(james, connor).
+parent(emily, connor).
+parent(isabella, liam).
+parent(jack, liam).
+parent(olivia, samantha).
+parent(connor, samantha).
+
+mather_in_law(M, H) :- woman(M), couple(H, K), parent(M, K)
