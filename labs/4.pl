79a3a68e5aa8d29989c63163eaa87de24f1e13da
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-07 04:37:08 +0200

    getEmplPosId

--- labs/4.pl
+++ labs/4.pl
@@ -1,110 +1,112 @@
-% employee (id, first_name, last_name, birthday, position_id)
-employee(1, "Oliver", "Smith", "6/21/1987", 7).
-employee(2, "George", "Johnson", "12/16/1987", 2).
-employee(3, "Harry", "Williams", "7/6/1997", 4).
-employee(4, "Noah", "Jones", "6/9/1984", 3).
-employee(5, "Jack", "Brown", "2/16/1992", 4).
-employee(6, "Charlie", "Davis", "3/28/1997", 6).
-employee(7, "Leo", "Miller", "6/6/1997", 1).
-employee(8, "Jacob", "Wilson", "2/16/1997", 6).
-employee(9, "Freddie", "Moore", "4/30/1980", 1).
-employee(10, "Alfie", "Taylor", "9/25/1993", 6).
-employee(11, "Archie", "Anderson", "10/19/1980", 5).
-employee(12, "Theo", "Thomas", "6/28/1982", 6).
-employee(13, "Oscar", "Jackson", "9/19/1987", 5).
-employee(14, "Arthur", "White", "4/17/1986", 5).
-employee(15, "Thomas", "Harris", "12/18/1992", 6).
-employee(16, "Logan", "Martin", "10/9/1990", 4).
-employee(17, "Henry", "Thompson", "1/16/1991", 5).
-employee(18, "Joshua", "Garcia", "5/21/1997", 4).
-employee(19, "James", "Martinez", "2/11/1998", 1).
-employee(20, "William", "Robinson", "10/29/1996", 3).
-employee(21, "Max", "Clark", "3/7/1993", 1).
-employee(22, "Isaac", "Rodriguez", "12/6/1995", 5).
-employee(23, "Lucas", "Lewis", "12/15/1981", 1).
-employee(24, "Ethan", "Lee", "5/8/1991", 1).
-employee(25, "Teddy", "Walker", "10/4/1985", 5).
-employee(26, "Finley", "Hall", "6/17/1990", 2).
-employee(27, "Mason", "Allen", "6/24/1990", 2).
-employee(28, "Harrison", "Young", "8/2/1993", 2).
-employee(29, "Hunter", "Hernandez", "4/5/1989", 5).
-employee(30, "Alexander", "King", "3/5/1980", 4).
-employee(31, "Daniel", "Wright", "8/29/1982", 4).
-employee(32, "Joseph", "Lopez", "5/27/1989", 4).
-employee(33, "Tommy", "Hill", "6/13/1984", 5).
-employee(34, "Arlo", "Scott", "9/19/1998", 4).
-employee(35, "Reggie", "Green", "9/5/1981", 5).
-employee(36, "Edward", "Adams", "12/21/1985", 6).
-employee(37, "Jaxon", "Baker", "9/18/1985", 6).
-employee(38, "Adam", "Gonzalez", "6/10/1994", 2).
-employee(39, "Sebastian", "Nelson", "6/10/1980", 4).
-employee(40, "Rory", "Carter", "11/28/1990", 2).
-employee(41, "Riley", "Mitchell", "4/23/1997", 3).
-employee(42, "Dylan", "Perez", "2/22/1989", 5).
-employee(43, "Elijah", "Roberts", "11/26/1993", 6).
-employee(44, "Carter", "Turner", "1/21/1983", 6).
-employee(45, "Albie", "Phillips", "7/23/1992", 1).
-employee(46, "Louie", "Campbell", "11/24/1997", 6).
-employee(47, "Toby", "Parker", "3/13/1990", 1).
-employee(48, "Benjamin", "Evans", "11/17/1981", 5).
-employee(49, "Reuben", "Edwards", "2/3/1980", 3).
-employee(50, "Jude", "Collins", "9/3/1989", 4).
-employee(51, "Olivia", "Stewart", "5/2/1981", 2).
-employee(52, "Amelia", "Sanchez", "10/3/1990", 4).
-employee(53, "Isla", "Morris", "11/24/1983", 6).
-employee(54, "Ava", "Rogers", "12/10/1993", 5).
-employee(55, "Emily", "Reed", "7/31/1997", 3).
-employee(56, "Sophia", "Cook", "8/5/1982", 6).
-employee(57, "Grace", "Morgan", "6/26/1988", 5).
-employee(58, "Mia", "Bell", "12/11/1990", 1).
-employee(59, "Poppy", "Murphy", "9/4/1984", 1).
-employee(60, "Ella", "Bailey", "9/27/1982", 3).
-employee(61, "Lily", "Rivera", "4/14/1986", 3).
-employee(62, "Evie", "Cooper", "8/14/1993", 3).
-employee(63, "Isabella", "Richardson", "10/13/1986", 4).
-employee(64, "Sophie", "Cox", "11/7/1984", 6).
-employee(65, "Ivy", "Howard", "2/20/1992", 3).
-employee(66, "Freya", "Ward", "8/9/1987", 3).
-employee(67, "Harper", "Torres", "11/30/1985", 4).
-employee(68, "Willow", "Peterson", "12/3/1996", 2).
-employee(69, "Charlotte", "Gray", "7/29/1990", 3).
-employee(70, "Jessica", "Ramirez", "1/9/1994", 4).
-employee(71, "Rosie", "James", "9/12/1997", 5).
-employee(72, "Daisy", "Watson", "4/27/1991", 5).
-employee(73, "Alice", "Brooks", "2/17/1980", 4).
-employee(74, "Elsie", "Kelly", "4/3/1998", 3).
-employee(75, "Sienna", "Sanders", "10/10/1986", 2).
-employee(76, "Florence", "Price", "10/4/1989", 1).
-employee(77, "Evelyn", "Bennett", "11/21/1980", 3).
-employee(78, "Phoebe", "Wood", "6/3/1987", 6).
-employee(79, "Aria", "Barnes", "7/14/1980", 6).
-employee(80, "Ruby", "Ross", "7/16/1997", 6).
-employee(81, "Isabelle", "Henderson", "9/21/1986", 6).
-employee(82, "Esme", "Coleman", "1/30/1990", 5).
-employee(83, "Scarlett", "Jenkins", "1/1/1990", 5).
-employee(84, "Matilda", "Perry", "5/7/1996", 4).
-employee(85, "Sofia", "Powell", "7/4/1983", 3).
-employee(86, "Millie", "Long", "4/25/1993", 5).
-employee(87, "Eva", "Patterson", "11/16/1990", 3).
-employee(88, "Layla", "Hughes", "10/13/1995", 6).
-employee(89, "Chloe", "Flores", "6/1/1989", 5).
-employee(90, "Luna", "Washington", "6/12/1996", 5).
-employee(91, "Maisie", "Butler", "1/20/1992", 6).
-employee(92, "Lucy", "Simmons", "11/10/1992", 4).
-employee(93, "Erin", "Foster", "8/24/1988", 3).
-employee(94, "Eliza", "Gonzales", "2/13/1986", 6).
-employee(95, "Ellie", "Bryant ", "1/15/1982", 4).
-employee(96, "Mila", "Alexander", "7/6/1986", 5).
-employee(97, "Imogen", "Russell", "8/27/1993", 3).
-employee(98, "Bella", "Griffin ", "5/10/1995", 5).
-employee(99, "Lola", "Diaz", "2/6/1996", 2).
-employee(100, "Molly", "Hayes", "7/17/1996", 3).
+getEmplPosId(EmplId, PosId) :- employee(EmplId, PosId, _, _, _), position(PosId, _, _).
+
+% employee (id, position_id, first_name, last_name, birthday)
+employee(1, 7, "Oliver", "Smith", "6/21/1987").
+employee(2, 2, "George", "Johnson", "12/16/1987").
+employee(3, 4, "Harry", "Williams", "7/6/1997").
+employee(4, 3, "Noah", "Jones", "6/9/1984").
+employee(5, 4, "Jack", "Brown", "2/16/1992").
+employee(6, 6, "Charlie", "Davis", "3/28/1997").
+employee(7, 1, "Leo", "Miller", "6/6/1997").
+employee(8, 6, "Jacob", "Wilson", "2/16/1997").
+employee(9, 1, "Freddie", "Moore", "4/30/1980").
+employee(10, 6, "Alfie", "Taylor", "9/25/1993").
+employee(11, 5, "Archie", "Anderson", "10/19/1980").
+employee(12, 6, "Theo", "Thomas", "6/28/1982").
+employee(13, 5, "Oscar", "Jackson", "9/19/1987").
+employee(14, 5, "Arthur", "White", "4/17/1986").
+employee(15, 6, "Thomas", "Harris", "12/18/1992").
+employee(16, 4, "Logan", "Martin", "10/9/1990").
+employee(17, 5, "Henry", "Thompson", "1/16/1991").
+employee(18, 4, "Joshua", "Garcia", "5/21/1997").
+employee(19, 1, "James", "Martinez", "2/11/1998").
+employee(20, 3, "William", "Robinson", "10/29/1996").
+employee(21, 1, "Max", "Clark", "3/7/1993").
+employee(22, 5, "Isaac", "Rodriguez", "12/6/1995").
+employee(23, 1, "Lucas", "Lewis", "12/15/1981").
+employee(24, 1, "Ethan", "Lee", "5/8/1991").
+employee(25, 5, "Teddy", "Walker", "10/4/1985").
+employee(26, 2, "Finley", "Hall", "6/17/1990").
+employee(27, 2, "Mason", "Allen", "6/24/1990").
+employee(28, 2, "Harrison", "Young", "8/2/1993").
+employee(29, 5, "Hunter", "Hernandez", "4/5/1989").
+employee(30, 4, "Alexander", "King", "3/5/1980").
+employee(31, 4, "Daniel", "Wright", "8/29/1982").
+employee(32, 4, "Joseph", "Lopez", "5/27/1989").
+employee(33, 5, "Tommy", "Hill", "6/13/1984").
+employee(34, 4, "Arlo", "Scott", "9/19/1998").
+employee(35, 5, "Reggie", "Green", "9/5/1981").
+employee(36, 6, "Edward", "Adams", "12/21/1985").
+employee(37, 6, "Jaxon", "Baker", "9/18/1985").
+employee(38, 2, "Adam", "Gonzalez", "6/10/1994").
+employee(39, 4, "Sebastian", "Nelson", "6/10/1980").
+employee(40, 2, "Rory", "Carter", "11/28/1990").
+employee(41, 3, "Riley", "Mitchell", "4/23/1997").
+employee(42, 5, "Dylan", "Perez", "2/22/1989").
+employee(43, 6, "Elijah", "Roberts", "11/26/1993").
+employee(44, 6, "Carter", "Turner", "1/21/1983").
+employee(45, 1, "Albie", "Phillips", "7/23/1992").
+employee(46, 6, "Louie", "Campbell", "11/24/1997").
+employee(47, 1, "Toby", "Parker", "3/13/1990").
+employee(48, 5, "Benjamin", "Evans", "11/17/1981").
+employee(49, 3, "Reuben", "Edwards", "2/3/1980").
+employee(50, 4, "Jude", "Collins", "9/3/1989").
+employee(51, 2, "Olivia", "Stewart", "5/2/1981").
+employee(52, 4, "Amelia", "Sanchez", "10/3/1990").
+employee(53, 6, "Isla", "Morris", "11/24/1983").
+employee(54, 5, "Ava", "Rogers", "12/10/1993").
+employee(55, 3, "Emily", "Reed", "7/31/1997").
+employee(56, 6, "Sophia", "Cook", "8/5/1982").
+employee(57, 5, "Grace", "Morgan", "6/26/1988").
+employee(58, 1, "Mia", "Bell", "12/11/1990").
+employee(59, 1, "Poppy", "Murphy", "9/4/1984").
+employee(60, 3, "Ella", "Bailey", "9/27/1982").
+employee(61, 3, "Lily", "Rivera", "4/14/1986").
+employee(62, 3, "Evie", "Cooper", "8/14/1993").
+employee(63, 4, "Isabella", "Richardson", "10/13/1986").
+employee(64, 6, "Sophie", "Cox", "11/7/1984").
+employee(65, 3, "Ivy", "Howard", "2/20/1992").
+employee(66, 3, "Freya", "Ward", "8/9/1987").
+employee(67, 4, "Harper", "Torres", "11/30/1985").
+employee(68, 2, "Willow", "Peterson", "12/3/1996").
+employee(69, 3, "Charlotte", "Gray", "7/29/1990").
+employee(70, 4, "Jessica", "Ramirez", "1/9/1994").
+employee(71, 5, "Rosie", "James", "9/12/1997").
+employee(72, 5, "Daisy", "Watson", "4/27/1991").
+employee(73, 4, "Alice", "Brooks", "2/17/1980").
+employee(74, 3, "Elsie", "Kelly", "4/3/1998").
+employee(75, 2, "Sienna", "Sanders", "10/10/1986").
+employee(76, 1, "Florence", "Price", "10/4/1989").
+employee(77, 3, "Evelyn", "Bennett", "11/21/1980").
+employee(78, 6, "Phoebe", "Wood", "6/3/1987").
+employee(79, 6, "Aria", "Barnes", "7/14/1980").
+employee(80, 6, "Ruby", "Ross", "7/16/1997").
+employee(81, 6, "Isabelle", "Henderson", "9/21/1986").
+employee(82, 5, "Esme", "Coleman", "1/30/1990").
+employee(83, 5, "Scarlett", "Jenkins", "1/1/1990").
+employee(84, 4, "Matilda", "Perry", "5/7/1996").
+employee(85, 3, "Sofia", "Powell", "7/4/1983").
+employee(86, 5, "Millie", "Long", "4/25/1993").
+employee(87, 3, "Eva", "Patterson", "11/16/1990").
+employee(88, 6, "Layla", "Hughes", "10/13/1995").
+employee(89, 5, "Chloe", "Flores", "6/1/1989").
+employee(90, 5, "Luna", "Washington", "6/12/1996").
+employee(91, 6, "Maisie", "Butler", "1/20/1992").
+employee(92, 4, "Lucy", "Simmons", "11/10/1992").
+employee(93, 3, "Erin", "Foster", "8/24/1988").
+employee(94, 6, "Eliza", "Gonzales", "2/13/1986").
+employee(95, 4, "Ellie", "Bryant ", "1/15/1982").
+employee(96, 5, "Mila", "Alexander", "7/6/1986").
+employee(97, 3, "Imogen", "Russell", "8/27/1993").
+employee(98, 5, "Bella", "Griffin ", "5/10/1995").
+employee(99, 2, "Lola", "Diaz", "2/6/1996").
+employee(100, 3, "Molly", "Hayes", "7/17/1996").
 
 % position (id, name, salary)
-position(1, "Frontend Developer", 2500);
-position(2, "Backend Developer", 3000);
-position(3, "QA", 1500);
-position(4, "HR", 1350);
-position(5, "PM", 4500);
-position(6, "CTO", 4500);
-position(7, "SEO", 10000);
+position(1, "Frontend Developer", 2500).
+position(2, "Backend Developer", 3000).
+position(3, "QA", 1500).
+position(4, "HR", 1350).
+position(5, "PM", 4500).
+position(6, "CTO", 4500).
+position(7, "SEO", 10000).
